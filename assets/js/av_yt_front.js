722ccd6c54e8114dcf35b5e91365651e105f492f
vedamak <rramroop@pm.me>
2020-06-03 08:36:40 +0200

    match all youtube embed URLs instead of relying on class name

--- assets/js/av_yt_front.js
+++ assets/js/av_yt_front.js
@@ -2,11 +2,23 @@
  * Subscribe widget front-end script
  */
 (function( $ ) {
- 
-    if( jQuery('.is-provider-youtube').length ) {
 
-		jQuery('.is-provider-youtube').css('margin-bottom', '0px');
-		jQuery('.is-provider-youtube').after( AV_YT_html );
-	}
+	jQuery('iframe').each(function(i, v) {
+
+		var elem 	= $(this);
+		var src 	= elem.attr('src');
+
+		if( (/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/embed\/.+/igm.test(src)) ) {
+
+			if( jQuery(elem).parents( 'figure.is-provider-youtube' ).length > 0 ) {
+
+				jQuery(elem).parents( 'figure.is-provider-youtube' ).after( AV_YT_html );
+			}
+			else {
+			
+				jQuery(elem).after( AV_YT_html );
+			}
+		}
+	});
      
 })( jQuery );
